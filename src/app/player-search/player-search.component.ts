e6686c7c142e1449b74047045cd734444c06ae69
BigBrownAfro <bigbrownafro@gmail.com>
2020-09-20 16:53:17 -0600

    Added error checking to player search inputs with user feedback.

--- src/app/player-search/player-search.component.ts
+++ src/app/player-search/player-search.component.ts
@@ -10,16 +10,19 @@ import { UserInfoCard } from '../models/UserInfoCard';
   styleUrls: ['./player-search.component.css']
 })
 export class PlayerSearchComponent implements OnInit {
-  @Input() accountSearchName:string = "";
+  @Input() playerSearchName:string = "";
   @Input() platform:string = "Platform";
   
   users:Array<string>;
+  errors:Array<string>; //user input errors for the user to see
+  errored:boolean; //whether an input contains an error or not
 
   allPlatforms:string[];
-  
 
   constructor(private destinyService:DestinyService) {
     this.users = new Array<string>();
+    this.errors = new Array<string>();
+    this.errored = false;
     this.allPlatforms = ["Steam", "Playstation", "Xbox", "Stadia"];
   }
 
@@ -27,9 +30,13 @@ export class PlayerSearchComponent implements OnInit {
   }
 
   searchForPlayer(){
+    if(!this.validateInputs()){
+      return;
+    }
+
     this.users = new Array<string>();
     this.users.push("Seaching...");
-    this.destinyService.getPlayerSearch(this.platform,this.accountSearchName)
+    this.destinyService.getPlayerSearch(this.platform,this.playerSearchName)
       .subscribe((res: BungieResponse) => {
         const playerSearchResults = res.Response as UserInfoCard[];
         this.users.pop();
@@ -44,5 +51,47 @@ export class PlayerSearchComponent implements OnInit {
 
   selectPlatform(platform:string){
     this.platform = platform;
+  }
+
+  validateInputs():boolean{
+    //clear errors
+    this.errors = new Array<string>();
+    this.errored = false;
+
+    //check fields
+    var isValid = true;
+    isValid = this.validatePlayerSearchName() && isValid;
+    isValid = this.validatePlatform() && isValid;;
+    return isValid;
+  }
+
+  validatePlayerSearchName():boolean{
+    var isValid = true;
+
+    //check player search name field for length out of bounds
+    if(this.playerSearchName.length < 1 || this.playerSearchName.length > 50){
+      isValid = false;
+
+      //update errors
+      this.errors.push("Username length invalid.");
+      this.errored = true;
+    }
+
+    return isValid;
+  }
+
+  validatePlatform():boolean{
+    var isValid = false;
+
+    //Check to see if entered platform is in the list of platforms
+    if(this.allPlatforms.includes(this.platform)){
+      isValid = true;
+    }else{
+      //update errors
+      this.errors.push("Please select a platform.");
+      this.errored = true;
+    }
+    
+    return isValid;
   }
 }
